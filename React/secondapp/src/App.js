038a44ab49d532a539404da07d4be468637ca5d1
knking <knr020498@gmail.com>
2023-05-27 15:48:07 +0530

    React app azios practice

--- React/secondapp/src/App.js
+++ React/secondapp/src/App.js
@@ -1,14 +1,25 @@
 
+import { useState } from "react";
 import Card from "./Card";
+import Axios from 'axios'
 
 const App = ()=>{
 
     const list = ["hitesh","anurag","krishna"]
+    const [details, setDetails] = useState({})
     
+    const fetcgDetails = async ()=>{
+        const {data} = await Axios.get('https://randomuser.me/api')
+        console.log(data)
+
+        const details = data.results[0]
+        setDetails(details)
+    }
     return(
         <div>
         Hello from App component 
-        <Card myname ="Hitesh" list={list}/>
+        <Card myname ="Hitesh" list={list}  details={details}/>
+        <button onClick={fetcgDetails}>Get Data</button>
     </div>
     )
    
--- React/secondapp/src/Card.jsx
+++ React/secondapp/src/Card.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Card = ({ myname, list }) => {
+const Card = ({ myname, list,details }) => {
 
     return (
         <>
             <div>This is Card </div>
-            
+            <h1>{details.gender}</h1>
+            <h2>{details.name?.first}</h2>
+            <h3>{details.location?.city}</h3>
             <ul>
                 {list.map((lst)=>(
                     <li key={lst}>{lst}</li>
