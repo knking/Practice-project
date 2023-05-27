10eda4ebbeb5c3302f892d418c1600b86920792d
knking <knr020498@gmail.com>
2023-05-27 10:39:21 +0530

    React app after deleting all default folder

--- React/secondapp/src/index.js
+++ React/secondapp/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
+const AppOne = ()=>(
+  <div>
+      Hello from App One component 
+  </div>
+)
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <AppOne/>
   </React.StrictMode>
 );
 
