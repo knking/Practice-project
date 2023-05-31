dd8720b0a232ed91723820d1a51ef5ae6892b0ff
knking <knr020498@gmail.com>
2023-05-31 21:13:51 +0530

    Weather app using react -> creating Todays forcast page

--- React/weatherapp/src/components/Main.jsx
+++ React/weatherapp/src/components/Main.jsx
@@ -2,18 +2,34 @@
 import React from 'react'
 import '../style/main.css'
 
+// use hooks to display current city name when app loded
+
 const Main = () => {
   return (
     <>
-    <div className="header flex">
-      <div className="left-section">
-        left
+      <div className="header flex">
+        <div className="left-section">
+          <form className='search-form'>
+            <input type='text' placeholder='Search for ctiies' id='search'></input>
+          </form>
+          <div className="city-name-section flex">
+            <div className="city-details flex">
+              <h1 className='city-name'>Noida</h1>
+              <p className="chance-of-rain">chance of rain: <span>0</span>%</p>
+              <div className="temperature">
+                <p className='temp'>31 c</p>
+              </div>
+            </div>
+            <div className="weatherlogo">
+              <img src="" alt="Temperature logo" srcset="" />
+            </div>
+          </div>
+        </div>
+        <div className="right-section">
+          right
+        </div>
       </div>
-      <div className="right-section">
-        right
-      </div>
-    </div>
-    
+
     </>
   )
 }
