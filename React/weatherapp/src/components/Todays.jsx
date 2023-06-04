b643251de2246dbe1ff534cdc3d00bef9856fb71
knking <knr020498@gmail.com>
2023-06-04 21:36:17 +0530

    Weather app using react -> updating daily weather condition

--- React/weatherapp/src/components/Todays.jsx
+++ React/weatherapp/src/components/Todays.jsx
@@ -1,34 +1,22 @@
 import React from 'react'
 import '../style/Todays.css'
-const Todays = ({sendForcast}) => {
+const Todays = ({ sendForcast }) => {
 
-const dd = sendForcast?.hour[3]
-const hr= sendForcast?.hour
+  const dd = sendForcast?.hour[3]
+  const hr = sendForcast?.hour
 
 
   const setTime = dd?.time;
-  const src= dd?.condition?.icon
+  const src = dd?.condition?.icon
   const temp = dd?.temp_c
   return (
     <>
-    <div className="todays-card">
+      <div className="todays-card">
+        <p className='todays-time'>{setTime.slice(11,16)}</p>
+        <img src={src} alt="weather" className='todays-img-logo' />
+        <p className='todays-temp'>{temp}<span>&#8451;</span></p>
+      </div>
 
-{
- hr.map((k,value)=>{
-  <>
-{
-  console.log(k)
-}
-<p className='todays-time'>{setTime}</p>
-  <img src={src} alt="weather" className='todays-img-logo'/>
-  <p className='todays-temp'>{temp}<span>&#8451;</span></p>
-  </>
-  
- })
-}
-     
-    </div>
-    
     </>
   )
 }
