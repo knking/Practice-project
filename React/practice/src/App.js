d21156aa58987558e358f04774f5fa130b1204f0
knking <knr020498@gmail.com>
2023-06-11 18:16:31 +0530

    react practice

--- React/practice/src/App.js
+++ React/practice/src/App.js
@@ -8,23 +8,29 @@ function App() {
 
 const [data,setData]= useState({})
 
-useEffect(()=>{
-  setCardValue();
-},[])
+// useEffect(()=>{
+//   // setCardValue();
+// },[])
 
-const setCardValue  = async ()=>{
-  const {data} = await Axios.get('https://randomuser.me/api/')
-  setData(data.results[0])
+// const setCardValue  = async ()=>{
+//   const {data} = await Axios.get('https://randomuser.me/api/')
+//   setData(data.results[0])
+//   console.log(data.results[0])
   
-  }
+//   }
+
+const setCardValue =  async ()=>{
+const { data } = await Axios.get('https://api.weatherapi.com/v1/forecast.json?key=d2d2708ebffb48bfb3d124500232505&q=bihar')
+console.log(data)
+}
 
   return (
     <div className="App d-flex">
       <button className="btn btn-info" onClick={setCardValue}>Get Card Data</button>
+    
+      {/* <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/>
       <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/>
-      <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/>
-      <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/>
-      <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/>
+      <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/> */}
       
     </div>
   );
--- React/practice/src/Card.jsx
+++ React/practice/src/Card.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
-const Card = ({imgSrc,title="i am card title",cardtext="iam content of card"}) => {
+const Card = ({imgSrc,location="bihar",current="44",forcast="rainy",hour="5"}) => {
   return (
     <>
     <div className="card" style={{width:"18rem"}}>
   <img src={imgSrc} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{cardtext}</p>
+    <h5 className="card-title">{location}</h5>
+    <p className="card-text">{current}</p>
+    <p className="card-text">{forcast}</p>
+    <p className="card-text">{hour}</p>
+
     <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
