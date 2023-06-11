73d4ab9e21a0f36230da088df0dea52e64039fa9
knking <knr020498@gmail.com>
2023-06-11 17:48:43 +0530

    react practice

--- React/practice/src/App.js
+++ React/practice/src/App.js
@@ -1,10 +1,30 @@
 
 import Axios from 'axios'
+import Card from './Card';
+import { useEffect, useState } from 'react';
+
+
 function App() {
+
+const [data,setData]= useState({})
+
+useEffect(()=>{
+  setCardValue();
+},[])
+
+const setCardValue  = async ()=>{
+  const {data} = await Axios.get('https://randomuser.me/api/')
+  setData(data.results[0])
+  
+  }
+
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    
+    <div className="App d-flex">
+      <button className="btn btn-info" onClick={setCardValue}>Get Card Data</button>
+      <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/>
+      <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/>
+      <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/>
+      <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/>
       
     </div>
   );
--- React/practice/src/Card.jsx
+++ React/practice/src/Card.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({imgSrc,title="i am card title",cardtext="iam content of card"}) => {
   return (
-    <div>Card</div>
+    <>
+    <div className="card" style={{width:"18rem"}}>
+  <img src={imgSrc} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{cardtext}</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </>
+   
   )
 }
 
