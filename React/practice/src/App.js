b94fe080245c0c8c07f7ce52b1bc7361dd8050e8
knking <knr020498@gmail.com>
2023-06-12 10:17:04 +0530

    react practice card data manupulation using api

--- React/practice/src/App.js
+++ React/practice/src/App.js
@@ -6,32 +6,37 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-const [data,setData]= useState({})
+  const [data, setData] = useState({})
 
-// useEffect(()=>{
-//   // setCardValue();
-// },[])
+  // useEffect(()=>{
+  //   // setCardValue();
+  // },[])
 
-// const setCardValue  = async ()=>{
-//   const {data} = await Axios.get('https://randomuser.me/api/')
-//   setData(data.results[0])
-//   console.log(data.results[0])
-  
-//   }
+  // const setCardValue  = async ()=>{
+  //   const {data} = await Axios.get('https://randomuser.me/api/')
+  //   setData(data.results[0])
+  //   console.log(data.results[0])
 
-const setCardValue =  async ()=>{
-const { data } = await Axios.get('https://api.weatherapi.com/v1/forecast.json?key=d2d2708ebffb48bfb3d124500232505&q=bihar')
-console.log(data)
-}
+  //   }
+
+  const setCardValue = async () => {
+    const { data } = await Axios.get('https://api.weatherapi.com/v1/forecast.json?key=d2d2708ebffb48bfb3d124500232505&q=bihar')
+    console.log(data)
+    console.log(data.location)
+    console.log(data.current)
+    console.log(data.forecast)
+    setData(data)
+  }
 
   return (
     <div className="App d-flex">
       <button className="btn btn-info" onClick={setCardValue}>Get Card Data</button>
-    
+
+      <Card imgSrc={data.forcast?.forecastday[0]?.condition?.icon} locationA={data.location?.name} currentA={data.current?.temp_c} forcastA={data.forcast?.forecastday[0]?.astro?.date} hourA={data.location?.localtime.slice(11,19)} />
       {/* <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/>
       <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/>
       <Card imgSrc={data.picture?.thumbnail} title={data.location?.city} cardtext={data.email}/> */}
-      
+
     </div>
   );
 }
--- React/practice/src/Card.jsx
+++ React/practice/src/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Card = ({imgSrc,location="bihar",current="44",forcast="rainy",hour="5"}) => {
+const Card = ({imgSrc,locationA="bihar",currentA="44",forcastA="rainy",hourA="5"}) => {
   return (
     <>
     <div className="card" style={{width:"18rem"}}>
   <img src={imgSrc} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{location}</h5>
-    <p className="card-text">{current}</p>
-    <p className="card-text">{forcast}</p>
-    <p className="card-text">{hour}</p>
+    <h5 className="card-title">Location: {locationA}</h5>
+    <p className="card-text">Current weather: {currentA}</p>
+    <p className="card-text">forecast: {forcastA}</p>
+    <p className="card-text">Date{hourA}</p>
 
     <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
