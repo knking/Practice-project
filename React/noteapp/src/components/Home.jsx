09eeda9fe2f9c0cf783b0a33c71352a034d1bdf2
knking <knr020498@gmail.com>
2023-06-13 19:26:13 +0530

    note app-> adding new note funcationality

--- React/noteapp/src/components/Home.jsx
+++ React/noteapp/src/components/Home.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/Home.css'
 import Task from './Task'
 
 const Home = () => {
+
+  const [titleData, setTitleData] = useState('')
+  const [textData, setTextData] = useState('')
+ const [task,setTask] = useState([])
+
+  const sendTask = (event) => {
+    event.preventDefault()
+
+    setTask([...task,{
+      title:titleData,
+      content:textData,
+    }])
+
+    console.log(task)
+
+  }
   return (
-   <>
-   <div className="container">
+    <>
+      <div className="container">
+        <form onSubmit={sendTask} className='form'>
+          <input type="text" name="title" id="" placeholder='Title Here' value={titleData} onChange={(event) => setTitleData(event.target.value)} />
+          <textarea name="textarea" id="" placeholder='Enter your Notes Here' value={textData} onChange={(event)=>setTextData(event.target.value)}>
+          </textarea>
+          <button type="submit">ADD</button>
+        </form>
+        {
+          task.map((item,value)=>(
+            <Task  key={value} title={item.title} content={item.content}/>
+          ))
+        }
+      </div>
 
-    <form action="" className='form'>
-
-      <input type="text" name="title" id="" placeholder='Title Here'/>
-      <textarea name="textarea" id="" placeholder='Enter your Notes Herea'>
-
-      </textarea>
-      <button type="submit">ADD</button>
-    </form>
-    <Task/>
-   </div>
-   
-   </>
+    </>
   )
 }
 
