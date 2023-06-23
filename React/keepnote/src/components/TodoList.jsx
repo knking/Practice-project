e1e178dc8a6db925744ff930c525f4f82a12268a
knking <knr020498@gmail.com>
2023-06-23 19:36:49 +0530

    keep note using react bootstrap

--- React/keepnote/src/components/TodoList.jsx
+++ React/keepnote/src/components/TodoList.jsx
@@ -5,10 +5,19 @@ import CreateTask from '../modals/CreateTask'
 const TodoList = () => {
 
     const [modal,setModal]= useState(false)
+    const [tasklist,setTasklist]= useState([])
 
     const toggle = ()=>{
         setModal(!modal)
     }
+
+    const saveTask = (taskObj)=>{
+let tempArr =tasklist;
+tempArr.push(taskObj);
+setTasklist(tempArr)
+setModal(false)
+    }
+
     return (
         <>
 
@@ -17,9 +26,11 @@ const TodoList = () => {
                 <button className='btn btn-primary mt-2' onClick={()=>setModal(true)}>Create task</button>
             </div>
             <div className="task-container">
-
+{
+    tasklist.map((obj)=><li>{obj.Name}</li>)
+}
             </div>
-            <CreateTask toggle={toggle} modal={modal}/>
+            <CreateTask toggle={toggle} modal={modal} save={saveTask}/>
         </>
     )
 }
--- React/keepnote/src/modals/CreateTask.jsx
+++ React/keepnote/src/modals/CreateTask.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const CreateTask = ({ modal, toggle }) => {
+const CreateTask = ({ modal, toggle,save }) => {
+
+    const [taskname,setTaskname] = useState('');
+    const [description,setDescription] = useState('')
+
+    const handleSave =()=>{
+        let taskObj ={}
+        taskObj["Name"]=taskname;
+        taskObj["Description"]=description;
+        save(taskObj)
+
+    }
     return (
         <>
             <Modal
@@ -15,16 +26,20 @@ const CreateTask = ({ modal, toggle }) => {
                 <ModalBody>
                     <form>
                         <div className="form-group">
-                            <input type='text' className='form-control'>
+                            <label>Task Name</label>
+                            <input type='text' className='form-control' value={taskname}
+                             onChange={(e)=>setTaskname(e.target.value)}>
                             </input>
                         </div>
+                        <label>Task Description</label>
                         <div className="form-group">
-                            <textarea  rows="5" className='form-control'></textarea>
+                            <textarea  rows="5" className='form-control' value={description}
+                            onChange={(e)=>setDescription(e.target.value)}></textarea>
                         </div>
                     </form>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={toggle}>
+                    <Button color="primary" onClick={handleSave} >
                         Create
                     </Button>
                     <Button color="secondary" onClick={toggle}>
