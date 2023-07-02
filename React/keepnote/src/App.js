f4a264c5de6645b115ca34fd25e88a613311ddd8
knking <knr020498@gmail.com>
2023-07-02 20:27:34 +0530

    notes app from scratch

--- React/keepnote/src/App.js
+++ React/keepnote/src/App.js
@@ -6,24 +6,42 @@ import { useState } from 'react';
 
 function App() {
 
-  const [singleTask,setSingleTask]= useState("")
+  const [singleTask, setSingleTask] = useState("")
+  const [addTask, setAddTask] = useState([])
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setAddTask([...addTask,singleTask])
+    setSingleTask(" ")
+  }
+
+  const handleDelete = (index)=>{
+const filteredArr = addTask.filter((val,idx)=>{
+return idx!==index
+})
+setAddTask(filteredArr)
+  }
 
   return (
     <div className='main'>
       <div className='notes'>
-      <h1>Add Your Note </h1>
-    <form>
-      <div className='form-div'>
-      <input type='text' placeholder='Add Notes Here....' value={singleTask} onChange={(e)=>(setSingleTask(e.target.value))}></input>
-      <button type='submit'>Add</button>
-      </div>
-    </form>
-    <h3>Your Notes</h3>
-      <Task addTask={singleTask}/>
-    
-      </div>
+        <h1>Add Your Note </h1>
+        <form onSubmit={handleSubmit}>
+          <div className='form-div'>
+            <input type='text' placeholder='Add Notes Here....' value={singleTask} onChange={(e) => (setSingleTask(e.target.value))}></input>
+            <button type='submit'>Add</button>
+          </div>
+        </form>
+        <h3>Your Notes</h3>
+        {
+          addTask.map((val,idx)=>(
+          <Task key={idx} addTask={val} deleteTask={handleDelete} index={idx}/>
+          ))
       
+        }
+
+      </div>
+
     </div>
   );
 }
