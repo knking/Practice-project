1a43737d776e4f2733f496bd6000d8c47776e48b
knking <knr020498@gmail.com>
2023-07-03 20:57:24 +0530

    edit  funcationality added

--- React/mynotes/src/App.js
+++ React/mynotes/src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
 
+  const [editId, setEditId] = useState(0)
+
   const handleSubmit = (e) => {
     e.preventDefault()
     if (todo !== '') {
@@ -17,21 +19,31 @@ function App() {
     setTodo("")
   }
 
+  const handleDelete = (id) => {
+    const deleteTodo = todos.filter((idx) => idx.id !== id)
+    setTodos([...deleteTodo])
+  }
+
+const handleEdit = (id)=>{
+const editTodo = todos.find((i)=> i.id ===id);
+setTodo(editTodo.todo)
+}
+
   return (
     <div className="App">
       <div className='container'>
         <h1>Todo List App</h1>
         <form className='todoForm' onSubmit={handleSubmit} >
           <input type='text' placeholder='Enter Todo here...' value={todo} onChange={(e) => setTodo(e.target.value)} />
-          <button type='submit'>Add</button>
+          <button type='submit'>{editId?"Update":"Add"}</button>
         </form>
         <ul className="altTodos">
           {
             todos.map((t) => (
               <li className='singleTodo'>
-                <span className='todoText'>{t.todo}</span>
-                <button>Edit</button>
-                <button>Delete</button>
+                <span className='todoText' key={t.id}>{t.todo}</span>
+                <button onClick={() => handleEdit(t.id)}>Edit</button>
+                <button onClick={() => handleDelete(t.id)}>Delete</button>
               </li>
             ))
           }
