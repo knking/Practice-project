61248f0c68c1f5c3b16bc6d20d6262e5077deade
knking <knr020498@gmail.com>
2023-07-05 18:41:27 +0530

    learning axios

--- React/axiosdemo/src/App.js
+++ React/axiosdemo/src/App.js
@@ -5,14 +5,18 @@ import './App.css'
 
 function App() {
 
-  const [country, setCountry] = useState([])
+  const [countries, setCountries] = useState([])
+
+  const[singleCountry, setSingleCountry] = useState();
+
+  const [cities,setCities] = useState([])
 
   const fetchCountry = async () => {
 
     try {
       const countries = await axios.get('https://countriesnow.space/api/v0.1/countries')
       console.log(countries.data.data)
-      setCountry(countries.data.data)
+      setCountries(countries.data.data)
     } catch (error) {
       console.log(error)
     }
@@ -23,33 +27,47 @@ function App() {
     fetchCountry();
   }, [])
 
+  const fetchCities = (country)=>{
+    setSingleCountry(country)
+
+    const findCities = countries.find((c)=>c.country === country)
+   setCities(findCities.cities)
+  }
   return (
     <div className="App">
       <div className='App-header'>
         <h1>Select Your Home Town</h1>
         <div>
-          {country && (
-            <select>
+          {countries && (
+            <select onChange={(e)=>fetchCities(e.target.value)} value={singleCountry}>
               <option disabled selected hidden>
                 Select country
               </option>
               {
-                country.map((country)=>(
-                  <option>{country.country}</option>
+                countries.map((country)=>(
+                  <option key={`${country.country}`} value={country.country}>{country.country}</option>
                 ))
               }
             </select>
           )
-
           }
 
-          <select>
-            <option disabled selected hidden>
-              Select City
-            </option>
-            <option>Bihar</option>
-            <option>UP</option>
-          </select>
+{
+  cities && <select>
+  <option disabled selected hidden>
+    Select City
+  </option>
+
+  {
+    cities.map((city)=>(
+<option key={city}>{city}</option>
+    ))
+  }
+  
+  
+</select>
+}
+          
           <button>GO</button>
         </div>
       </div>
