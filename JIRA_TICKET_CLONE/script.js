43beaca9593af13358eb576952b1dc775c3a4194
knking <knr020498@gmail.com>
2023-10-13 19:57:31 +0530

    jira-tikcet-management->ticket creation fn added

--- JIRA_TICKET_CLONE/script.js
+++ JIRA_TICKET_CLONE/script.js
@@ -2,6 +2,8 @@
 const addbtn = document.querySelector(".add-btn")
 let addFlag = false
 let modalCont = document.querySelector(".modal-cont")
+let mainCont = document.querySelector(".main-cont")
+
 addbtn.addEventListener("click",function (e){
     addFlag =!addFlag
     if(addFlag){
@@ -10,3 +12,23 @@ addbtn.addEventListener("click",function (e){
         modalCont.style.display="none"
     }
 })
+
+modalCont.addEventListener('keydown', (e) =>{
+    
+    if(e.key ==="shift"){
+        console.log("hi")
+        createTicket();
+    }
+})
+
+function createTicket(){
+    let ticketCont = document.createElement("div");
+    ticketCont.setAttribute("class",'ticket-cont')
+    ticketCont.innerHTML= `
+    <div class="ticket-color"></div>
+    <div class="ticket-id">Smaple_id</div>
+    <div class="task-area">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, excepturi. Nihil ipsa tempora, quis atque dignissimos repellendus adipisci magni sint. In minus mollitia nulla sequi voluptate magnam sit. Delectus, nam.
+     </div>
+    `;
+    mainCont.appendChild(ticketCont);
+}
