f5043d8435be2c59a12a6cc3994c910aa755de83
knking <knr020498@gmail.com>
2023-10-25 07:52:40 +0530

    jira-tikcet-management->ticket edit functionality added

--- JIRA_TICKET_CLONE/script.js
+++ JIRA_TICKET_CLONE/script.js
@@ -1,9 +1,32 @@
 
 const addbtn = document.querySelector(".add-btn")
+const removeBtn = document.querySelector(".remove-btn")
 let addFlag = false
+let removeFlag = false
 let modalCont = document.querySelector(".modal-cont")
 let mainCont = document.querySelector(".main-cont")
 let textArea = document.querySelector(".textarea-cont")
+let allPriortyColor = document.querySelectorAll(".priorty-color")
+let lockElem = document.querySelector(".ticket-lock")
+
+let lock = "fa-lock";
+let unlock = "fa-lock-open"
+
+let colors = ["lightpink","lightblue","lightgreen","black"]
+let defalutlColor=colors[colors.length -1]
+console.log("color", defalutlColor)
+
+//listner for all prirorty color
+
+allPriortyColor.forEach((colorElem,idx ) => {
+    colorElem.addEventListener('click',(e)=>{
+        allPriortyColor.forEach((prirprtyColorElement,i)=>{
+            prirprtyColorElement.classList.remove("border")
+        })
+        colorElem.classList.add("border")
+        defalutlColor = colorElem.classList[0]
+    })
+});
 
 addbtn.addEventListener("click",function (e){
     addFlag =!addFlag
@@ -14,9 +37,27 @@ addbtn.addEventListener("click",function (e){
     }
 })
 
+removeBtn.addEventListener('click',(e)=>{
+    removeFlag =!removeFlag
+})
+
+//generate random id 
+
+function generateRandomCharacterId(length) {
+    const chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  }
+
+
 modalCont.addEventListener('keydown',(e)=>{
     if(e.key==="Shift"){
-        createTicket()
+        let res = generateRandomCharacterId(5)
+        createTicket(defalutlColor,textArea.value,res)
         modalCont.style.display="none";
         addFlag=false;
         textArea.value= "";
@@ -25,14 +66,61 @@ modalCont.addEventListener('keydown',(e)=>{
 })
 
 
-function createTicket(){
+function createTicket(ticketColor, ticketTask, ticketId){
     let ticketCont = document.createElement("div");
     ticketCont.setAttribute("class",'ticket-cont')
     ticketCont.innerHTML= `
-    <div class="ticket-color"></div>
-    <div class="ticket-id">Smaple_id</div>
-    <div class="task-area">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, excepturi. Nihil ipsa tempora, quis atque dignissimos repellendus adipisci magni sint. In minus mollitia nulla sequi voluptate magnam sit. Delectus, nam.
-     </div>
+    <div class="ticket-color ${ticketColor}"></div>
+    <div class="ticket-id">${ticketId}</div>
+    <div class="task-area">${ticketTask}</div>
+    <div class="ticket-lock">
+        <i class="fa-solid fa-lock"></i>
+    </div>
     `;
     mainCont.appendChild(ticketCont);
+    handleRemove(ticketCont)
+    handleLock(ticketCont)
+    handleColor(ticketCont)
+}
+
+function handleRemove(ticket){
+if(removeFlag){
+    ticket.remove()
+}
+}
+
+function handleLock(){
+    let tikcetLockElem =  document.querySelector(".ticket-lock")
+  let ticketLock = tikcetLockElem.children[0];
+  let ticketTaskArea = document.querySelector(".task-area")
+  ticketLock.addEventListener('click',(e)=>{
+    if(ticketLock.classList.contains(lock)){
+        ticketLock.classList.remove(lock)
+        ticketLock.classList.add(unlock)
+        ticketTaskArea.setAttribute("contenteditable","true")
+    }else{
+        ticketLock.classList.remove(unlock)
+        ticketLock.classList.add(lock)
+        ticketTaskArea.setAttribute("contenteditable","false")
+    }
+     
+  })
+}
+function handleColor(ticket){
+let ticketColor= ticket.querySelector(".ticket-color")
+ticketColor.addEventListener('click',(e)=>{
+
+let currentTicketColor =ticketColor.classList[1]
+
+let currentTicketColorIdx=colors.findIndex((color)=>{
+return currentTicketColor === color;
+})
+
+currentTicketColorIdx++;
+console.log(currentTicketColor, currentTicketColorIdx)
+let newTicketColorIdx= currentTicketColorIdx % colors.length;
+let newTicketColor = colors[newTicketColorIdx]
+ticketColor.classList.remove(currentTicketColor)
+ticketColor.classList.add(newTicketColor)
+})
 }
