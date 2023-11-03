d58ba98fdac31455729dc9c6cd21da7297dbf2cf
knking <knr020498@gmail.com>
2023-11-03 10:21:08 +0530

    React counter app finished

--- React/day2/02counterapp/src/App.jsx
+++ React/day2/02counterapp/src/App.jsx
@@ -1,22 +1,32 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
+  let [count,setCount]=useState(1)
   
-  let count =1;
-  const addvalue = ()=>{
-    console.log("Clicked",count)
-    count=count+1
-  }
+  // const addvalue = ()=>{
+  //   if(count>=20) return
+  //   else{
+  //     setCount(count+1)
+  //   }
+  // }
+
+  // function decrementValue(){
+  //   if(count<=0) count=""
+  //   else{
+  //     count=count-1
+  //   }
+  // }
 
   return (
     <>
       <h1>Chai Aur React</h1>
       <h2>Counter value : {count}</h2>
-      <button onClick={addvalue}>Increament</button>
-      <button onClick={()=>console.log("Hi")}>Decrement</button>
+      <button onClick={()=>count>=10?"" : setCount(count+1)}>Increament</button>
+      <button onClick={()=>count <=0?"": setCount(count -1)}>Decrement</button>
     </>
   )
 }
