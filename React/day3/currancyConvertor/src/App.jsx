33581631b72dba7e983b5c9cb64726118fdcd312
knking <knr020498@gmail.com>
2023-11-05 22:26:41 +0530

    React currency converter project

---
+++ React/day3/currancyConvertor/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500'>Currancy Changer</h1>
+    </>
+  )
+}
+
+export default App
